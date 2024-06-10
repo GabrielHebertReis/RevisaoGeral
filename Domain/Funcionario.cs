495b86c0c60f2495c5c9d626f645be5cafee5be6
GabrielHebertReis <gabrielhebert68@gmail.com>
2024-06-10 08:45:06 -0300

    criação do "domain"

---
+++ Domain/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Funcionario : Base
+    {
+        public string Nome { get; set; }
+        public string Func { get; set; }
+        public int Salario { get; set; }
+        public Base Base { get; set; }
+
+    }
+}
